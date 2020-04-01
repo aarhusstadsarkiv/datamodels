a3d28ea716f7c65e5f659550f742d64520abd019
Nina Jensen <jnik@aarhus.dk>
2020-04-01 14:53:21 +0200

    Add a root validator checking the relationship between PUID & signature

--- datamodels/identification.py
+++ datamodels/identification.py
@@ -16,3 +16,19 @@ class Identification(BaseModel):
     puid: Optional[str] = Field(...)
     signature: Optional[str] = Field(...)
     warning: Optional[str]
+
+    @root_validator
+    def check_puid_sig(cls, values: Dict[Any, Any]) -> Dict[Any, Any]:
+        """Validate that a PUID cannot have an empty signature
+        or vice versa."""
+
+        puid = values["puid"]
+        signature = values["signature"]
+
+        if puid is not None and signature is None:
+            raise ValueError(f"Signature missing for PUID {puid}.")
+
+        if signature is not None and puid is None:
+            raise ValueError(f"PUID missing for signature {signature}.")
+
+        return values
