c232a262f288aeef422f1721620b8f122a9ae2ac
Nina Jensen <jnik@aarhus.dk>
2020-04-02 12:43:42 +0200

    Add tests of File datamodel

---
+++ tests/test_file.py
@@ -0,0 +1,22 @@
+# -----------------------------------------------------------------------------
+# Imports
+# -----------------------------------------------------------------------------
+
+from pydantic import ValidationError
+
+from datamodels import File
+
+# -----------------------------------------------------------------------------
+# Tests
+# -----------------------------------------------------------------------------
+
+
+class TestInit:
+    def test_required_fields(self):
+        pass
+
+    def test_optional_fields(self):
+        pass
+
+    def test_validators(self):
+        pass
