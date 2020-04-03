6813689a4ed929c9c9138a7a0b8ff6626931f8d7
Nina Jensen <jnik@aarhus.dk>
2020-04-03 13:53:10 +0200

    Move test_file fixture from conftest.py to test_file.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,10 +18,3 @@ import pytest
 def temp_dir(tmpdir_factory):
     temp_dir: str = tmpdir_factory.mktemp("temp_dir")
     return Path(temp_dir)
-
-
-@pytest.fixture
-def test_file(temp_dir):
-    test_file: Path = temp_dir / "test.txt"
-    test_file.write_text("This is a test file.")
-    return test_file
--- tests/test_file.py
+++ tests/test_file.py
@@ -2,9 +2,25 @@
 # Imports
 # -----------------------------------------------------------------------------
 
+from pathlib import Path
+
 from pydantic import ValidationError
 
-from datamodels import File
+import pytest
+from datamodels import File, Identification
+from datamodels._internals import OverwriteWarning, size_fmt
+
+# -----------------------------------------------------------------------------
+# Fixtures
+# -----------------------------------------------------------------------------
+
+
+@pytest.fixture
+def test_file(temp_dir):
+    test_file: Path = temp_dir / "test.txt"
+    test_file.write_text("This is a test file.")
+    return test_file
+
 
 # -----------------------------------------------------------------------------
 # Tests
