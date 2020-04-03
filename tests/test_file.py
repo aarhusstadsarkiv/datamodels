8a2b64e009d2e7e60e829a505f69e16268055133
Nina Jensen <jnik@aarhus.dk>
2020-04-03 13:57:14 +0200

    Add initialisation tests for File data model

--- tests/test_file.py
+++ tests/test_file.py
@@ -28,11 +28,22 @@ def test_file(temp_dir):
 
 
 class TestInit:
-    def test_required_fields(self):
-        pass
+    def test_required_fields(self, test_file):
+        file = File(path=test_file)
+        assert file.path == test_file
+        assert file.name == test_file.name
+        assert file.ext == test_file.suffix.lower()
+        assert file.size == size_fmt(test_file.stat().st_size)
+        assert file.checksum is None
+        assert file.identification is None
 
-    def test_optional_fields(self):
-        pass
+    def test_optional_fields(self, test_file):
+        file_identification = Identification(
+            puid="fmt/test", signature="Test signature"
+        )
+        file = File(
+            path=test_file, checksum="abc", identification=file_identification,
+        )
+        assert file.checksum == "abc"
+        assert file.identification == file_identification
 
-    def test_validators(self):
-        pass
