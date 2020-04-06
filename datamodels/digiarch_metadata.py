3693223f2548bbf8cb9af1d7ae56e29eede47973
Nina Jensen <jnik@aarhus.dk>
2020-04-06 15:08:27 +0200

    Add model definition and validators

--- datamodels/digiarch_metadata.py
+++ datamodels/digiarch_metadata.py
@@ -16,7 +16,23 @@ from pydantic import BaseModel, validator
 class DigiarchMetadata(BaseModel):
     """Digiarch metadata data model."""
 
-    last_run: Optional[datetime]
+    last_run: datetime = datetime.min
     processed_directory: Path
     file_count: int
     total_size: str
+    empty_subdirectories: Optional[List[Path]]
+    several_files: Optional[List[Path]]
+    identification_warnings: Optional[int]
+    duplicates: Optional[int]
+
+    @validator("last_run", pre=True, always=True)
+    def set_last_run(cls, last_run: datetime) -> datetime:
+        if last_run == datetime.min:
+            last_run = datetime.now()
+        return last_run
+
+    @validator("processed_directory")
+    def path_must_be_dir(cls, processed_directory: Path) -> Path:
+        if not processed_directory.resolve().is_dir():
+            raise ValueError("Directory does not exist")
+        return processed_directory.resolve()
