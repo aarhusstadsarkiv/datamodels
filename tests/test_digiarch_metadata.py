c8682f5d91bf74a311ea127fbfe0bc0bd1ec84f5
Nina Jensen <jnik@aarhus.dk>
2020-04-07 11:44:49 +0200

    Update validation tests with frozen time to test invocation of last_run

--- tests/test_digiarch_metadata.py
+++ tests/test_digiarch_metadata.py
@@ -4,13 +4,13 @@
 
 from datetime import datetime
 from pathlib import Path
-from unittest.mock import patch
 
 from pydantic import ValidationError
-import pytest
 
+import pytest
 from datamodels import DigiarchMetadata
 from datamodels._internals import size_fmt
+from freezegun import freeze_time
 
 # -----------------------------------------------------------------------------
 # Tests
@@ -63,17 +63,26 @@ class TestValidators:
     file_count = 0
     size = size_fmt(0)
 
+    # Time is frozen, so datetime.now() will
+    # return datetime(2020, 3, 23)
+    @freeze_time("2020-03-23")
     def test_set_last_run(self, temp_dir):
-        # TODO: Mock with FreezeGun
-        # https://github.com/spulec/freezegun
-        _time = datetime.now()
-        with patch("datetime.datetime.now", return_value=_time):
-            metadata = DigiarchMetadata(
-                processed_directory=temp_dir,
-                file_count=self.file_count,
-                total_size=self.size,
-            )
-        assert metadata.last_run == _time
+        # Test with no last_run parameter
+        metadata = DigiarchMetadata(
+            processed_directory=temp_dir,
+            file_count=self.file_count,
+            total_size=self.size,
+        )
+        assert metadata.last_run == datetime(2020, 3, 23)
+
+        # With last run set
+        metadata = DigiarchMetadata(
+            last_run="2010-02-16 00:00",
+            processed_directory=temp_dir,
+            file_count=self.file_count,
+            total_size=self.size,
+        )
+        assert metadata.last_run == datetime(2010, 2, 16)
 
     def test_path_must_be_dir(self):
         with pytest.raises(ValidationError):
